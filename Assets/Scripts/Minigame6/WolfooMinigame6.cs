d070c5367a91a24c3342ef3efa92129bc461b387
XHieuhust <92824756+XHieuhust@users.noreply.github.com>
2023-11-20 07:30:44 +0700

    Scene6.2

--- Assets/Scripts/Minigame6/WolfooMinigame6.cs
+++ Assets/Scripts/Minigame6/WolfooMinigame6.cs
@@ -17,6 +17,7 @@ public class WolfooMinigame6 : MonoBehaviour
     //System check falling down pit
     private float hightYonGround;
     public bool isFallingDown;
+    bool isJumpingUp;
 
     // system check on ground
     RaycastHit2D rayLeft;
@@ -33,8 +34,10 @@ public class WolfooMinigame6 : MonoBehaviour
     [SerializeField] private LayerMask layer;
     private float lengthRay = 0.2f;
 
-    [SerializeField] private float JumpBackForce;
-
+    //boost
+    public bool isBoosting;
+    [SerializeField] private float speedBoost;
+    [SerializeField] private float timeBoost;
     private void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -70,21 +73,42 @@ public class WolfooMinigame6 : MonoBehaviour
             if (CheckOnGround())
             {
                 rigid.AddForce(Vector2.up * forceJump);
+                skeleton.AnimationState.SetAnimation(0, "Jump_Hight", false);
             }
         }
     }
 
     private void Move()
     {
-        if (!isHitted && !isFallingDown)
+        if (!isHitted && !isFallingDown && !isJumpingUp)
         {
-            speed = speedNormal;
-        }
-        else
+            if (!isBoosting)
+            {
+                speed = speedNormal;
+            }
+            if (isBoosting)
+            {
+                speed = speedBoost;
+                if (transform.position.y <= hightYonGround)
+                {
+                    transform.position = new Vector3(transform.position.x, hightYonGround + 0.05f, transform.position.z);
+                    rigid.velocity = new Vector2(speed, 0);
+                }
+            }
+        }else
         {
             speed = 0;
         }
         if (!CheckNearOb()) rigid.velocity = new Vector2(speed, rigid.velocity.y);
+        SetAnimRun();
+    }
+
+    private void SetAnimRun()
+    {
+        if (transform.position.y - hightYonGround >= 0.1f && rigid.velocity.y < 0 && !isHitted)
+        {
+            skeleton.AnimationState.SetAnimation(0, "Run_ninja", true);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -94,28 +118,65 @@ public class WolfooMinigame6 : MonoBehaviour
             RaycastHit2D rayCheck = Physics2D.Raycast(PosRayRight.position, Vector2.right, lengthRay, layer);
             if (rayCheck)
             {
-                collision.gameObject.GetComponent<Collider2D>().enabled = false;
-                Hitted();
+                if (!isBoosting)
+                {
+                    collision.gameObject.GetComponent<Collider2D>().enabled = false;
+                    Hitted();
+                }
+                else
+                {
+                    collision.gameObject.GetComponent<ObsacleMinigame6>().Fly();
+                }
             }
         }
 
         if (collision.gameObject.CompareTag("Pit"))
         {
             transform.position = new Vector3(collision.transform.position.x, transform.position.y, transform.position.z);
-            JumpBack();
+            float sizePit = collision.collider.bounds.size.x/2;
+            JumpBack(sizePit);
+        }
+
+        if (collision.gameObject.CompareTag("Buff"))
+        {
+            StartCoroutine(nameof(StartBoost));
         }
     }
 
-    private void JumpBack()
+    IEnumerator StartBoost()
     {
-        StartCoroutine(nameof(StartJumpBack));
+        isBoosting = true;
+        yield return new WaitForSeconds(timeBoost);
+        isBoosting = false;
     }
 
-    IEnumerator StartJumpBack()
+    private void JumpBack(float sizePit)
     {
-        yield return new WaitForSeconds(1f);
-        rigid.AddForce(Vector2.up * JumpBackForce);
+        StartCoroutine(nameof(StartJumpBack), sizePit);
+    }
+
+    IEnumerator StartJumpBack(float sizePit)
+    {
+        yield return new WaitForSeconds(1.5f);
+        Vector2 oldPos = transform.position;
+        Vector2 newPos = new Vector3(transform.position.x + sizePit, hightYonGround + 0.5f, 0);
+        float eslapsed = 0;
+        float seconds = 0.75f;
         isFallingDown = false;
+        isJumpingUp = true;
+        skeleton.AnimationState.SetAnimation(0, "Jump_Hight", false);
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        while (eslapsed <= seconds)
+        {
+            eslapsed += Time.deltaTime;
+            transform.position = Vector2.Lerp(oldPos, newPos, eslapsed / seconds);
+            yield return new WaitForEndOfFrame();
+        }
+        transform.position = newPos;
+        isJumpingUp = false;
+        GetComponent<Collider2D>().enabled = true;
+        GetComponent<Rigidbody2D>().isKinematic = false;
     }
     private void Hitted()
     {
@@ -133,7 +194,7 @@ public class WolfooMinigame6 : MonoBehaviour
 
     void CheckFallOnPit()
     {
-        if ((transform.position.y <= hightYonGround - 1f) && !isFallingDown)
+        if ((transform.position.y <= hightYonGround - 1f) && !isFallingDown && !isJumpingUp)
         {
             isFallingDown = true;
         }
