14335e5aed2b434261147c6bcea00c4fb3081e41
Hieu <hieutqp2003@gmail.com>
2023-12-13 15:33:00 +0700

    Update BubbleKids.cs

--- Assets/Scripts/Minigame3/Scene3.2/BubbleKids.cs
+++ Assets/Scripts/Minigame3/Scene3.2/BubbleKids.cs
@@ -5,20 +5,24 @@ using UnityEngine.UI;
 
 public class BubbleKids : MonoBehaviour
 {
+    RectTransform rect;
     float startScale;
     Image bubbleImage;
-    Animator animator;
     int curBg;
     Vector3 startPos;
     [SerializeField] List<Sprite> spriteBubbles;
     [SerializeField] Transform posHint;
+    Vector3 pivot1;
+    Vector3 pivot2;
     private void Start()
     {
+        rect = GetComponent<RectTransform>();
         bubbleImage = GetComponent<Image>();
-        animator = GetComponent<Animator>();
         startScale = transform.localScale.x;
         transform.localScale = Vector3.zero;
         startPos = transform.position;
+        pivot1 = rect.pivot;
+        pivot2 = new Vector3(1, 0.5f);
     }
 
     private void OnEnable()
@@ -40,6 +44,7 @@ public class BubbleKids : MonoBehaviour
 
     IEnumerator StartScaleUp()
     {
+        //rect.pivot = pivot1;
         float eslapsed = 0;
         float seconds = 0.5f;
         transform.position = startPos;
@@ -52,7 +57,6 @@ public class BubbleKids : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
         transform.localScale = new Vector3(end, end, end);
-        //animator.Play("Hint1");
 
     }
 
@@ -60,16 +64,16 @@ public class BubbleKids : MonoBehaviour
     {
         if (curBg < 3)
         {
+            rect.pivot = pivot2;
             Vector3 newPosHint = new Vector3(Camera.main.transform.position.x + Camera.main.orthographicSize * Camera.main.aspect - 0.1f, posHint.position.y, posHint.position.z);
             transform.position = newPosHint;
             bubbleImage.sprite = spriteBubbles[1];
             curBg++;
+            
         }
         else
         {
             ScaleUp();
         }
-
-
     }
 }
